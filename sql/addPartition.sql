41189faef5a92b14c040616548b424be1d5204ed
Gopinathan Balasubramanian <b.gopinathan@gmail.com>
2017-01-31 09:33:44 -0600

    Changes until Jan 30

---
+++ sql/addPartition.sql
@@ -0,0 +1,1 @@
+use tradingdb;alter table stock_statistics add partition(xchange='NASDAQ',trade_date='2017-01-30');
---
+++ sql/create_Stock_statistics_table.sql
@@ -0,0 +1,23 @@
+use tradingdb;
+
+-- create an external hive table to manage the statistics.
+
+create external table if not exists stock_statistics 
+       ( symbol        string, 
+         day_range     string,
+         volume        bigint, 
+         4wk_range     string,
+         total_volume  bigint,
+         avg_30_volume bigint,
+         avg_10_volume bigint, 
+         chg_30_volume decimal(9,2)
+       )
+       comment 'This is a statistics mined from Stock Exchange End data'
+       partitioned by (xchange string,trade_date string) 
+       row format delimited 
+       fields terminated by "," 
+       lines terminated by "\n" 
+       null defined as "*"
+       location '/data/processed/stock_statistics/'
+       ;
+
