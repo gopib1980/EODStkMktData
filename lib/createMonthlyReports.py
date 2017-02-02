6a5b4868b4d15804dd9fdc6fead2894b3f0ffb51
Gopinathan Balasubramanian <b.gopinathan@gmail.com>
2017-02-01 22:07:50 -0600

    Changes from Feb 1st

--- lib/createMonthlyReports.py
+++ lib/createMonthlyReports.py
@@ -30,12 +30,35 @@ sqlContext = HiveContext(sc)
 Top10HighVolumeRpt = "/data/processed/monthly_reports/"+sys.argv[1]+"/"+sys.argv[2]+"/Top10HighVolumeRpt"
 Top10LowVolumeRpt = "/data/processed/monthly_reports/"+sys.argv[1]+"/"+sys.argv[2]+"/Top10LowVolumeRpt"
 
+Top10HighPrcChgDayRpt = "/data/processed/monthly_reports/"+sys.argv[1]+"/"+sys.argv[2]+"/Top10HighPrcChgDayRpt"
+Top10HighPrcChgMonRpt = "/data/processed/monthly_reports/"+sys.argv[1]+"/"+sys.argv[2]+"/Top10HighPrcChgMonRpt"
+Top10LowPrcChgMonRpt  = "/data/processed/monthly_reports/"+sys.argv[1]+"/"+sys.argv[2]+"/Top10LowPrcChgMonRpt"
 
-top10HighVolume = sqlContext.sql("select xchange,'"+sys.argv[2]+"',symbol,sum(volume) as total_volume from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'  group by xchange,symbol order by total_volume desc limit 10")
 
-top10HighVolume.save(Top10HighVolumeRpt,"com.databricks.spark.csv")
+#top10HighVolume = sqlContext.sql("select xchange,'"+sys.argv[2]+"',symbol,sum(volume) as total_volume from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'  group by xchange,symbol order by total_volume desc limit 10")
 
-top10LowVolume = sqlContext.sql("select xchange,'"+sys.argv[2]+"',symbol,sum(volume),'"+sys.argv[2]+"' as total_volume from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'  group by xchange,symbol order by total_volume limit 10")
+#top10HighVolume.save(Top10HighVolumeRpt,"com.databricks.spark.csv")
 
-top10LowVolume.save(Top10LowVolumeRpt,"com.databricks.spark.csv")
+#top10LowVolume = sqlContext.sql("select xchange,'"+sys.argv[2]+"',symbol,sum(volume) as total_volume from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'  group by xchange,symbol order by total_volume limit 10")
 
+#top10LowVolume.save(Top10LowVolumeRpt,"com.databricks.spark.csv")
+
+#top10HighPrcChgDay = sqlContext.sql("select xchange,'"+sys.argv[2]+"',symbol,trade_date,open,close,cast(100*(close-open)/open as decimal(5,2)) as prcchgpct from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'  order by prcchgpct desc limit 10")
+
+#top10HighPrcChgDay.save(Top10HighPrcChgDayRpt,"com.databricks.spark.csv")
+
+firstDayofMon = sqlContext.sql("select a.xchange,a.symbol,a.trade_date,a.open,a.high,a.low,a.close,a.volume from tradingdb.eod_stock_data a inner join (select symbol,min(trade_date) as max_trade_date from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"' group by symbol) b on a.symbol=b.symbol and a.trade_date = b.max_trade_date and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'")
+firstDayofMon.registerTempTable("firstDayofMon")
+
+
+lastDayofMon = sqlContext.sql("select a.xchange,a.symbol,a.trade_date,a.open,a.high,a.low,a.close,a.volume from tradingdb.eod_stock_data a inner join (select symbol,max(trade_date) as max_trade_date from tradingdb.eod_stock_data where xchange='"+sys.argv[1]+"' and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"'   group by symbol) b on a.symbol=b.symbol and a.trade_date = b.max_trade_date and date_format(trade_date,'YYYY-MM') = '"+sys.argv[2]+"' ")
+lastDayofMon.registerTempTable("lastDayofMon")
+
+
+top10HighPrcChgMonRpt = sqlContext.sql("select a.xchange,'"+sys.argv[2]+"',a.symbol,100*(a.close-b.close)/a.close as changePct from firstDayofMon a, lastDayofMon b where a.symbol=b.symbol order by changePct desc limit 10")
+
+top10HighPrcChgMonRpt.write.save(Top10HighPrcChgMonRpt,"com.databricks.spark.csv")
+
+top10LowPrcChgMonRpt = sqlContext.sql("select a.xchange,'"+sys.argv[2]+"',a.symbol,100*(a.close-b.close)/a.close as changePct from firstDayofMon a, lastDayofMon b where a.symbol=b.symbol order by changePct limit 10")
+
+top10LowPrcChgMonRpt.write.save(Top10LowPrcChgMonRpt,"com.databricks.spark.csv")
